05ea879fddb11607d85367c109a9e2b1269635b0
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-09-06 16:02:42 -0300

    alteração de codigo

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log(dados);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById
+    ("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    console.log(campoPesquisa);
+    
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada item de dados da pesquisa
+    for (let dado of dados) {
+        console.log(dado.titulo.includes(campoPesquisa))
+        // Concatena HTML para criar um novo item de resultado
+        resultados += `
+            <div class="item-resultado">
+            <h2>
+                <a href="${dado.link}" target="_blank">${dado.titulo}</a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+        `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
+
+
+
